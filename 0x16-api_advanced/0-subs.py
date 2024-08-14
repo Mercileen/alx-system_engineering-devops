74929ddca0e544f495c7764b03fbfe25f691c420
Mercileen <kamwindamercy@gmail.com>
2024-08-14 12:57:10 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers) for a given subreddit"""
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers from the Reddit API"""
-    r = requests.get(r'https://www.reddit.com/r/{}/about.json'
-                     .format(subreddit), headers={'User-agent': 'x'},
-                     allow_redirects=False)
-    if r.status_code != 200:
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If the subreddit is invalid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyBot/0.1'}  # Custom User-Agent to avoid Too Many Requests error
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data.get('data', {}).get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-    json = r.json()
-    data = json.get('data')
-    return data.get('subscribers', 0)
